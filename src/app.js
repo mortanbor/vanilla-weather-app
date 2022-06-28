bcdc0379f28548fc20436a52376023d1fadc2925
mortanbor <42118390+mortanbor@users.noreply.github.com>
2022-06-29 01:22:48 +0200

    Add Current Date in JS (New date)

--- src/app.js
+++ src/app.js
@@ -1,3 +1,31 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let dayOrder = [date.getDay()];
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[dayOrder];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function showTemperature(response) {
   console.log(response.data);
 
@@ -11,6 +39,8 @@ function showTemperature(response) {
   humidity.innerHTML = response.data.clouds.all;
   let wind = document.querySelector("#wind");
   wind.innerHTML = Math.round(response.data.wind.speed);
+  let dateElement = document.querySelector("#currentdate");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "14a082666cde45212a997627987614d0";
