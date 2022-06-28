f120125580416dc5c6aebb38b1825e4476d8cf95
mortanbor <42118390+mortanbor@users.noreply.github.com>
2022-06-29 01:43:49 +0200

    Added relevant icon

--- src/app.js
+++ src/app.js
@@ -41,6 +41,12 @@ function showTemperature(response) {
   wind.innerHTML = Math.round(response.data.wind.speed);
   let dateElement = document.querySelector("#currentdate");
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "14a082666cde45212a997627987614d0";
